de975efe0795cfe96e5ce78f12564fbb0ab389ff
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-11 22:13:24 +0100

    Create zadanie1.js

---
+++ 20211106/zadanie1.js
@@ -0,0 +1,38 @@
+// 1) Create an iffe that returns an object with fields: function setValue(), function showValue() and function reverseValue(). Calling functions either logs the value or reverse it in an object. If value was not provided yet or is empty showValue function is to return information about that. 			
+// 	Value can be type string or number. reverseValue():  If number do (*(-1)), if string reverse it.  Closure pattern.		
+// 1) Utwórz iffe, który zwraca obiekt z polami: funkcja setValue(), funkcja showValue() i funkcja reverseValue().  Wywoływanie funkcji albo rejestruje wartość, albo odwraca ją w obiekcie. Jeżeli wartość nie została jeszcze podana lub jest pusta funkcja showValue  ma zwrócić informację o tym.			
+         
+             var input = 'kot';		
+		
+let iife = (function () {		
+		
+function showValue(input) {		
+        if(showValue.arguments.length == 0 || input == undefined || input == ''){		
+        return ('true')};}		
+        		
+function setValue(input) {		
+        if(arguments.length == 0 || input == undefined || input == ''){		
+        return ('true')} else { return input;};}		
+		
+function reverseValue(input){		
+        if(typeof input === 'number'){		
+                input = input*(-1);		
+                return ('Reversed number is = ' + input)		
+                } else {		
+                if(typeof input === 'string' && input !== 'undefined'){		
+                return ('Reversed string is = ' + (input.split('').reverse().join('')));		
+                };  // odwraca string		
+                };		
+            };		
+		
+    return {		
+    showValue,		
+    setValue,		
+    reverseValue,		
+           };		
+    		
+})(); // koniec IIFE		
+		
+if (iife.showValue(input) == 'true'){console.log('No value in function!')};		
+if (iife.setValue(input) !== 'true'){console.log('Value is = ' + iife.setValue(input));};		
+if (iife.setValue(input) !== 'true'){console.log(iife.reverseValue(input));};		
