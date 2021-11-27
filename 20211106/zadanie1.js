7edd6e01e29d83551b3ab444d0ad247c3975912c
TomaszJaniszta <92643157+TomaszJaniszta@users.noreply.github.com>
2021-11-27 15:46:15 +0100

    Update zadanie1.js

--- 20211106/zadanie1.js
+++ 20211106/zadanie1.js
@@ -2,9 +2,46 @@
 // 	Value can be type string or number. reverseValue():  If number do (*(-1)), if string reverse it.  Closure pattern.		
 // 1) Utwórz iffe, który zwraca obiekt z polami: funkcja setValue(), funkcja showValue() i funkcja reverseValue().  Wywoływanie funkcji albo rejestruje wartość, albo odwraca ją w obiekcie. Jeżeli wartość nie została jeszcze podana lub jest pusta funkcja showValue  ma zwrócić informację o tym.			
          
-             var input = 'kot';		
+//              var input = 'kot';		
 		
-let iife = (function () {		
+// let iife = (function () {		
+		
+// function showValue(input) {		
+//         if(showValue.arguments.length == 0 || input == undefined || input == ''){		
+//         return ('true')};}		
+//         		
+// function setValue(input) {		
+//         if(arguments.length == 0 || input == undefined || input == ''){		
+//         return ('true')} else { return input;};}		
+		
+// function reverseValue(input){		
+//         if(typeof input === 'number'){		
+//                 input = input*(-1);		
+//                 return ('Reversed number is = ' + input)		
+//                 } else {		
+//                 if(typeof input === 'string' && input !== 'undefined'){		
+//                 return ('Reversed string is = ' + (input.split('').reverse().join('')));		
+//                 };  // odwraca string		
+//                 };		
+//             };		
+		
+//     return {		
+//     showValue,		
+//     setValue,		
+//     reverseValue,		
+//            };		
+//     		
+// })(); // koniec IIFE		
+		
+// if (iife.showValue(input) == 'true'){console.log('No value in function!')};		
+// if (iife.setValue(input) !== 'true'){console.log('Value is = ' + iife.setValue(input));};		
+// if (iife.setValue(input) !== 'true'){console.log(iife.reverseValue(input));};		
+
+//************************************************************** */
+
+             var input = '';		
+		
+let iife = (function (input) {		
 		
 function showValue(input) {		
         if(showValue.arguments.length == 0 || input == undefined || input == ''){		
@@ -31,8 +68,8 @@ function reverseValue(input){
     reverseValue,		
            };		
     		
-})(); // koniec IIFE		
+})(input); // koniec IIFE		
 		
 if (iife.showValue(input) == 'true'){console.log('No value in function!')};		
-if (iife.setValue(input) !== 'true'){console.log('Value is = ' + iife.setValue(input));};		
-if (iife.setValue(input) !== 'true'){console.log(iife.reverseValue(input));};		
+console.log('Value is = ' + iife.setValue(input));	
+console.log(iife.reverseValue(input));	
