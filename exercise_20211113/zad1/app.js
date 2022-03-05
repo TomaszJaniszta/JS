c9713fd97d0692f56406468ebe78222ffc030dd8
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-03-05 13:35:57 +0100

    Create app.js

---
+++ exercise_20211113/zad1/app.js
@@ -0,0 +1,3 @@
+let stringTest = 'stringTest';
+let extendedString = stringTest + stringTest.split('').reverse().join('') ;
+console.log("Extended text 'stringTest' by its reversed value is: " +  extendedString);
