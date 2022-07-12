23523529fd4722c2f3b0e0cdbf412575df079f7b
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-07-12 10:20:03 +0200

    Delete app.js

--- exercise_20211113/zad2/app.js
+++
@@ -1,6 +0,0 @@
-let numberTest = 100;
-function reverseNumber(num){
-  return String(num).split("").reverse().join("")
-}
-let extendedNumber = parseFloat(numberTest + reverseNumber(numberTest)) ;
-console.log("Extended number '100' by its reversed value is: " +  extendedNumber);
