1b1f671ec05c40ae26c79dafd63f6c165a955a43
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-07-12 10:20:01 +0200

    Delete app.js

--- exercise_20211113/zad1/app.js
+++
@@ -1,3 +0,0 @@
-let stringTest = 'stringTest';
-let extendedString = stringTest + stringTest.split('').reverse().join('') ;
-console.log("Extended text 'stringTest' by its reversed value is: " +  extendedString);
